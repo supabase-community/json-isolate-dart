6e02e72f6d3d3d90f168e283a719b22f57b44486
Vinzent <vinzent03@proton.me>
2022-11-26 11:05:39 +0100

    feat: move src files

--- example/yet_another_json_isolate_example.dart
+++ example/yet_another_json_isolate_example.dart
@@ -1,6 +1,14 @@
 import 'package:yet_another_json_isolate/yet_another_json_isolate.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  final isolate = YAJsonIsolate();
+
+  await isolate.initialize();
+
+  final json = await isolate.decode('{"a": 1, "b": 2}');
+  print(json);
+
+  final str = await isolate.encode(json);
+  print(str);
+  isolate.dispose();
 }
